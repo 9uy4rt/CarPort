c0b170ce1a246fcf547bf59f3e825b7c975fdbe3
9uy4rt <108713543+9uy4rt@users.noreply.github.com>
2022-08-29 15:01:54 +0900

    Update Car.java

--- src/cpp/Car.java
+++ src/cpp/Car.java
@@ -1,15 +1,26 @@
 package cpp;
+
 public interface Car {
 	public void gear(String gear);
+
 	public void speed();
+
 	public void tire(String tire);
+
 	public void win(String win);
+
 	public void winF(String winF);
+
 	public void winR(String winR);
+
 	public void winL(String winL);
+
 	public void wiper(String wiper);
+
 	public void light(String light);
+
 	public void display();
+
 	public void qBreak();
-		
+
 }
